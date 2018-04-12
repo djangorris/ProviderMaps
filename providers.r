1379c04988534ce97a9922995a8ba30b0374fe14
djangorris <lukkyjay@gmail.com>
2018-04-12 12:37:00 -0600

    Map perfectly outlining Colorado is working. The color and shape attributes of the geom_point is also working.

--- providers.r
+++ providers.r
@@ -6,6 +6,7 @@ library(tidyverse)
 library(ggmap)
 library(DT)
 library(knitr)
+library(ggplot2)
 
 providers <- read_excel("Anthem.xlsm", sheet = "IndividualProviders1", skip = 2962, 
                         col_names = c("Street", "City", "State", "Zip"), col_types = c("skip", "skip", "skip", "skip", "skip", "skip", "skip", "skip", "text", "skip", "text", "text", "skip", "text", "skip"))
@@ -14,3 +15,14 @@ providers$locations <- paste0(providers$Street, ", ", providers$City, ", ", prov
 
 # This function geocodes a location (find latitude and longitude) using the Google Maps API
 geo <- geocode(location = providers$locations, output="latlon", source="google")
+
+# Bringing over the longitude and latitude data
+providers$lon <- geo$lon
+providers$lat <- geo$lat
+
+col_lon <- c(-109, -102)
+col_lat <- c(36.86204, 41.03)
+bbox <- make_bbox(col_lon, col_lat, f=0.05)
+co_map <- get_map(bbox, maptype="toner", source = "stamen")
+ggmap(co_map) +
+  geom_point(aes(lon, lat), color = "blue", shape = 21, data = providers)
