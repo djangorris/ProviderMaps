b1e5488a07cf0fd9afc59180cfbf1c6ff57f1838
djangorris <lukkyjay@gmail.com>
2018-04-12 18:11:46 -0600

    Added ability to read both sheets, then bind_rows, then remove duplicates. Still need to convert the provider type to a factor. Also added another file called combining_carriers.r - which is for combining all of the carriers into one tibble. Also added serff xlsm files to gitignore. Also updated main gitignore.

--- providers.r
+++ providers.r
@@ -1,28 +1,48 @@
 install.packages("readxl")
 install.packages("tidyverse")
 install.packages('ggmap')
+install.packages('purrr')
 library(readxl)
 library(tidyverse)
 library(ggmap)
+library(purrr)
 library(DT)
 library(knitr)
 library(ggplot2)
 
-providers <- read_excel("Anthem.xlsm", sheet = "IndividualProviders1", skip = 2962, 
-                        col_names = c("Street", "City", "State", "Zip"), col_types = c("skip", "skip", "skip", "skip", "skip", "skip", "skip", "skip", "text", "skip", "text", "text", "skip", "text", "skip"))
+providers1 <- read_excel("Anthem.xlsm", sheet = "IndividualProviders1", skip = 2, 
+                        col_names = c("NPI", "Specialty", "Street", "City", "State", "Zip"), 
+                        col_types = c("text", "skip", "skip", "skip", "skip", "skip", "skip", 
+                                      "text", "text", "skip", "text", "text", "skip", "text", "skip"))
 
+providers2 <- read_excel("Anthem.xlsm", sheet = "IndividualProviders2", skip = 2, 
+                         col_names = c("NPI", "Specialty", "Street", "City", "State", "Zip"), 
+                         col_types = c("text", "skip", "skip", "skip", "skip", "skip", "skip", 
+                                       "text", "text", "skip", "text", "text", "skip", "text", "skip"))
+
+# COMBINE THE TWO SHEETS
+providers_bind <- bind_rows(providers1, providers2)
+
+# NEED TO REMOVE DUPLICATES
+providers = providers_bind[!duplicated(providers_bind$NPI),]
+
+# PROVIDER TYPE TO FACTOR
+
+# ADD CONCATENATED LOCATIONS COLUMN
 providers$locations <- paste0(providers$Street, ", ", providers$City, ", ", providers$State, ", ", providers$Zip)
 
-# This function geocodes a location (find latitude and longitude) using the Google Maps API
+# geocodes a location (find latitude and longitude) using the Google Maps API
 geo <- geocode(location = providers$locations, output="latlon", source="google")
 
 # Bringing over the longitude and latitude data
 providers$lon <- geo$lon
 providers$lat <- geo$lat
 
+# DEFINING THE MAP
 col_lon <- c(-109, -102)
 col_lat <- c(36.86204, 41.03)
 bbox <- make_bbox(col_lon, col_lat, f=0.05)
-co_map <- get_map(bbox, maptype="toner", source = "stamen")
+co_map <- get_map(bbox, maptype="toner-lite", source = "stamen")
+# DISPLAY MAP AND ADD DATA POINTS
 ggmap(co_map) +
   geom_point(aes(lon, lat), color = "blue", shape = 21, data = providers)
