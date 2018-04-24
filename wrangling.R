fc63f70c071f3352623570cde05df8618eb0697d
djangorris <lukkyjay@gmail.com>
2018-04-24 14:43:17 -0600

    moved mapping code from wrangling file to new directory Mapping as prototype

--- wrangling.R
+++ wrangling.R
@@ -18,36 +18,3 @@ ALL_county_specialty_count$County <- str_to_title(ALL_county_specialty_count$Cou
 ### EXPORTING ###
 write_csv(ALL_county_specialty_count, path = "Indiv/ALL_county_specialty_count.csv")
 write_csv(ALL_statewide_specialty_count, path = "Indiv/ALL_statewide_specialty_count.csv")
-
-### GROUPING ###
-################
-##### TOTALS #####
-
-
-
-
-
-
-
-
-
-###########
-###########
-###########
-# < **need to research geocoding options** >
-# geocodes a location (find latitude and longitude) using the Google Maps API
-# geo <- geocode(location = medical_providers$locations, output="latlon", source="google")
-##############
-
-# Bringing over the longitude and latitude data
-medical_providers$lon <- geo$lon
-medical_providers$lat <- geo$lat
-
-# DEFINING THE MAP
-col_lon <- c(-109, -102)
-col_lat <- c(36.86204, 41.03)
-bbox <- make_bbox(col_lon, col_lat, f=0.05)
-co_map <- get_map(bbox, maptype="toner-lite", source = "stamen")
-# DISPLAY MAP AND ADD DATA POINTS
-ggmap(co_map) +
-  geom_point(aes(lon, lat), color = "blue", shape = 21, data = medical_providers)
