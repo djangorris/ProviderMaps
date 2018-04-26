411429836da941930144c97b1c4b50d6d8143d7e
djangorris <lukkyjay@gmail.com>
2018-04-26 10:36:28 -0600

    Added caption and save to single_county_total.R. Changed the county_selected variable to selected_county to make it consistent with other scripts

--- Group_Filter_Plot/single_county_single_specialty.R
+++ Group_Filter_Plot/single_county_single_specialty.R
@@ -1,12 +1,12 @@
 ###### SINGLE COUNTY BY SINGLE SPECIALTY #####
 selected_county <- "Larimer"
 selected_specialty <- "003 Internal Medicine"
-COUNTY_ALL <- ALL_county_specialty_count %>% 
-  group_by(Specialty, County) %>% 
+COUNTY_ALL <- ALL_county_specialty_count %>%
+  group_by(Specialty, County) %>%
   filter(Specialty == selected_specialty, County == selected_county)
 # plot
-cols <- c("Anthem BCBS" = "blue", 
-          "Bright" = "deeppink3", 
+cols <- c("Anthem BCBS" = "blue",
+          "Bright" = "deeppink3",
           "Cigna" = "forestgreen",
           "Denver Health" = "navyblue",
           "Kaiser" = "deepskyblue2",
@@ -16,4 +16,9 @@ ggplot(COUNTY_ALL, aes(x = Carrier, y = nproviders, fill = Carrier)) +
   xlab("Insurance Carrier") +
   ylab("Number of Providers") +
   ggtitle(str_c("Providers listed as ", '"', selected_specialty, '" in ', selected_county, " County ")) +
-  scale_fill_manual(values = cols)
+  scale_fill_manual(values = cols) +
+  labs(caption = "  Graphic by Jay Norris / @lukkyjay  |  Source: SERFF") +
+  theme(plot.margin = margin(5, 5, 5, 5),
+        plot.caption = element_text(size = 8, color = "grey", hjust = 0)) +
+  ggsave(filename = paste0(here("/"), "Plots/", selected_county, "_", selected_specialty, ".png"),
+         width = 12, height = 8, dpi = 1200)
--- Group_Filter_Plot/single_county_total.R
+++ Group_Filter_Plot/single_county_total.R
@@ -1,10 +1,10 @@
 ##### SINGLE COUNTY TOTAL  #####
-county_selected <- "Summit"
-SINGLE_COUNTY <- ALL_county_specialty_count %>% 
-  filter(County == county_selected) %>% 
+selected_county <- "Summit"
+SINGLE_COUNTY <- ALL_county_specialty_count %>%
+  filter(County == county_selected) %>%
   summarise(Total = sum(nproviders, na.rm = TRUE))
-cols <- c("Anthem BCBS" = "blue", 
-          "Bright" = "deeppink3", 
+cols <- c("Anthem BCBS" = "blue",
+          "Bright" = "deeppink3",
           "Cigna" = "forestgreen",
           "Denver Health" = "navyblue",
           "Kaiser" = "deepskyblue2",
@@ -13,5 +13,10 @@ ggplot(SINGLE_COUNTY, aes(x = Carrier, y = Total, fill = Carrier)) +
   geom_bar(position="dodge", stat="identity") +
   xlab("Insurance Carrier") +
   ylab("Number of Providers") +
-  ggtitle(str_c(county_selected, " County Medical Providers")) +
-  scale_fill_manual(values = cols)
+  ggtitle(str_c(selected_county, " County Medical Providers")) +
+  scale_fill_manual(values = cols) +
+  labs(caption = "  Graphic by Jay Norris / @lukkyjay  |  Source: SERFF") +
+  theme(plot.margin = margin(5, 5, 5, 5),
+        plot.caption = element_text(size = 8, color = "grey", hjust = 0)) +
+  ggsave(filename = paste0(here("/"), "Plots/", selected_county, ".png"),
+         width = 12, height = 8, dpi = 1200)
