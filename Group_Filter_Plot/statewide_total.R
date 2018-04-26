bfe037cc707ea946a903ead66c73570a23e76504
djangorris <lukkyjay@gmail.com>
2018-04-26 10:25:36 -0600

    Added Plots directory and edited statewide_total.R to save file there. Added Plots directory to gitignore.

--- Group_Filter_Plot/statewide_total.R
+++ Group_Filter_Plot/statewide_total.R
@@ -1,9 +1,9 @@
 ### ALL MEDICAL DOCTORS STATEWIDE ###
-STATEWIDE_ALL <- ALL_statewide_specialty_count %>% 
-  group_by(Carrier) %>% 
+STATEWIDE_ALL <- ALL_statewide_specialty_count %>%
+  group_by(Carrier) %>%
   summarise(Total = sum(count, na.rm = TRUE))
-cols <- c("Anthem BCBS" = "blue", 
-          "Bright" = "deeppink3", 
+cols <- c("Anthem BCBS" = "blue",
+          "Bright" = "deeppink3",
           "Cigna" = "forestgreen",
           "Denver Health" = "navyblue",
           "Kaiser" = "deepskyblue2",
@@ -16,6 +16,6 @@ ggplot(STATEWIDE_ALL, aes(x = Carrier, y = Total, fill = Carrier)) +
   scale_fill_manual(values = cols) +
   labs(caption = "  Graphic by Jay Norris / @lukkyjay  |  Source: SERFF") +
   theme(plot.margin = margin(5, 5, 5, 5),
-        plot.caption = element_text(size = 8, color = "grey", hjust = 0)) + 
-  ggsave(filename = paste0(here("/"), "statewide_all.png"),
+        plot.caption = element_text(size = 8, color = "grey", hjust = 0)) +
+  ggsave(filename = paste0(here("/"), "Plots/statewide_all.png"),
          width = 12, height = 8, dpi = 1200)
