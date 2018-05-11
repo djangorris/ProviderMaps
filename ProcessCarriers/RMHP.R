46a1efc3b3b6c91ea6399c9ac73d3b2edd4a6ceb
djangorris <lukkyjay@gmail.com>
2018-05-11 11:00:41 -0600

    First maps created and they look good.

--- ProcessCarriers/RMHP.R
+++ ProcessCarriers/RMHP.R
@@ -2,35 +2,24 @@ file <- "RMHP.xlsm"
 path <- paste0("Indiv/", file)
 ext_removed <- tools::file_path_sans_ext(file)
 carrier_name <- gsub("_", " ", ext_removed, fixed=TRUE)
-providers <- read_excel(path, sheet = "IndividualProviders1", skip = 2, 
-                        col_names = c("NPI", "Prefix", "First_Name", "Last_Name", "Physician", "Specialty", "Street", "City", "State", "County", "Zip"), 
-                        col_types = c("text", "text", "text", "skip", "text", "skip", "text", 
+providers <- read_excel(path, sheet = "IndividualProviders1", skip = 2,
+                        col_names = c("NPI", "Prefix", "First_Name", "Last_Name", "Physician", "Specialty", "Street", "City", "State", "County", "Zip"),
+                        col_types = c("text", "text", "text", "skip", "text", "skip", "text",
                                       "text", "text", "skip", "text", "text", "text", "text", "skip"))
-# REMOVE "000 OTHER IF IT'S WITH ANOTHER SPECIALTY" AND ", "
-providers$Specialty <- str_replace(providers$Specialty, "000 OTHER, ", "")
-providers$Specialty <- str_replace(providers$Specialty, "001 General Practice, 002 Family Medicine", "002 Family Medicine")
-providers$Specialty <- str_replace(providers$Specialty, "001 General Practice, ", "")
-providers$Specialty <- str_replace(providers$Specialty, "002 Family Medicine, ", "")
-providers$Specialty <- str_replace(providers$Specialty, "003 Internal Medicine, ", "")
-providers$Specialty <- str_replace(providers$Specialty, ", 015 General Surgery", "")
-providers$Specialty <- str_replace(providers$Specialty, ", 101 Pediatrics - Routine/Primary Care", "")
-providers$Specialty <- str_replace(providers$Specialty, "015 General Surgery, ", "")
+# ADD CONCATENATED NAME + LOCATION COLUMN TO REMOVE DUPLICATES
 providers$name_location <- paste0(providers$First_Name, " ", providers$Last_Name, ": ", providers$Street, ", ", providers$City, ", ", providers$State, ", ", providers$Zip)
 # REMOVE DUPLICATES
 providers = providers[!duplicated(providers$name_location),]
 # ADD CONCATENATED LOCATIONS COLUMN FOR GEOCODING LATER
 providers$locations <- paste0(providers$Street, ", ", providers$City, ", ", providers$State, ", ", providers$Zip)
-# PROVIDER SPECIALTY TO FACTOR
-providers$Specialty <- as.factor(providers$Specialty)
-# COUNTY TO FACTOR
-providers$County <- as.factor(providers$County)
+# SEPARATE COLLAPSED CATEGORIES INTO MULTIPLE ROWS
+providers <- separate_rows(providers, Specialty, sep = ",", convert = TRUE)
 # ADD "CARRIER" COLUMN
 providers$Carrier <- carrier_name
-providers$Carrier <- as.factor(providers$Carrier)
 # SEPARATE DENTAL PROVIDERS FROM MEDICAL PROVIDERS
-RMHP_dental_providers <- filter(providers, 
-                                 Specialty %in% c("Dental - General", 
-                                                  "Dental - Periodontist", 
+RMHP_dental_providers <- filter(providers,
+                                 Specialty %in% c("Dental - General",
+                                                  "Dental - Periodontist",
                                                   "Dental - Orthodontist",
                                                   "Dental - Endodontist"))
 RMHP_medical_providers <- filter(providers,
@@ -39,14 +28,12 @@ RMHP_medical_providers <- filter(providers,
                                                     "Dental - Orthodontist",
                                                     "Dental - Endodontist"))
 # COUNT THE NUMBER OF PROVIDERS IN EACH CATEGORY STATEWIDE
-RMHP_statewide_specialty_count <- RMHP_medical_providers %>% 
-  group_by(Carrier, Specialty) %>% 
+RMHP_statewide_specialty_count <- RMHP_medical_providers %>%
+  group_by(Carrier, Specialty) %>%
   summarise(count = n()) %>%
   arrange(-count)
 # COUNT THE NUMBER OF PROVIDERS IN EACH CATEGORY IN EACH COUNTY
-RMHP_county_specialty_count <- RMHP_medical_providers %>% 
-  group_by(Carrier, Specialty, County) %>% 
+RMHP_county_specialty_count <- RMHP_medical_providers %>%
+  group_by(Carrier, Specialty, County) %>%
   summarise(nproviders = n()) %>%
   arrange(-nproviders)
-# ADD CONCATENATED LOCATIONS COLUMN
-RMHP_medical_providers$locations <- paste0(RMHP_medical_providers$Street, ", ", RMHP_medical_providers$City, ", ", RMHP_medical_providers$State, ", ", RMHP_medical_providers$Zip)
--- requirements.R
+++ requirements.R
@@ -23,23 +23,23 @@ library(bigtabulate)
 
 # Provider Map theme
 theme_provider_maps <- theme(plot.margin = margin(10, 10, 10, 10),
-                        plot.title = element_text(family = "Arial Narrow",
-                                                  color="grey10",
-                                                  size = 18,
-                                                  hjust=0),
-                        axis.text.x = element_text(size = 14,
-                                                    face = "bold"), # Carrier name on bar plots
-                        axis.text.y = element_text(size = 14,
-                                                   face = "bold"), # Y axis on plots
-                        strip.text.x = element_text(size = 14,
-                                                    face = "bold"), # Facel label
-                        panel.spacing.x=unit(0.2, "lines"),
-                        panel.spacing.y=unit(0.5, "lines"),
-                        legend.position = "none",
-                        plot.caption = element_text(family = "Arial",
-                                                    size = 10,
-                                                    color = "grey50",
-                                                    hjust = 0.5))
+                             plot.title = element_text(family = "Arial Narrow",
+                                                       color="grey10",
+                                                       size = 18,
+                                                       hjust=0),
+                             # axis.text.x = element_text(size = 14,
+                             #                            face = "bold"), # Carrier name label on bar charts
+                             # axis.text.y = element_text(size = 12,
+                             #                            face = "bold"), # Y axis label on charts
+                             strip.text.x = element_text(size = 14,
+                                                         face = "bold"), # Facet label
+                             panel.spacing.x=unit(0.2, "lines"),
+                             panel.spacing.y=unit(0.5, "lines"),
+                             legend.position = "none",
+                             plot.caption = element_text(family = "Arial",
+                                                         size = 10,
+                                                         color = "grey50",
+                                                         hjust = 0.5))
 
 font_import() # import all your fonts
 fonts() #get a list of fonts
