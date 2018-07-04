67811458770febe02bbc3ebd7f588e9c8fd70f74
djangorris <lukkyjay@gmail.com>
2018-07-04 15:57:30 -0600

    Completed plots 001, 002, 003.

---
+++ Mapping/Plotting/internal_medicine_003.R
@@ -0,0 +1,43 @@
+# REPLACE:
+# internal_medicine_003 (target geocoded variable name)
+# 003 Internal Medicine (copy/paste from TOTAL_Specialty_Count spreadsheet on Desktop)
+
+# GET COUNT FOR EACH CARRIER
+selected_specialty <- "003 Internal Medicine"
+clean_selected_specialty <- str_replace_all(selected_specialty, " ", "_")
+statewide_count_internal_medicine_003 <- ALL_statewide_specialty_count %>%
+  filter(Specialty == selected_specialty)
+# DEFINING THE MAP
+col_lon <- c(-109, -102)
+col_lat <- c(36.86204, 41.03)
+bbox <- make_bbox(col_lon, col_lat, f=0.05)
+co_map <- get_map(bbox, maptype="toner-lite", source = "stamen")
+# DISPLAY MAP AND ADD DATA POINTS
+ggmap(co_map, extent = "device") +
+  # geom_density2d(data = internal_medicine_003,
+  #                aes(x = lon, y = lat),
+  #                size = 0.3) +
+  # stat_density2d(data = internal_medicine_003,
+  #                aes(x = lon, y = lat, fill = ..level.., alpha = ..level..),
+  #                size = 0.01,
+  #                bins = 10,
+  #                geom = "polygon") +
+  # scale_fill_gradient(low = "tomato", high = "green4") +
+  # scale_alpha(range=c(0,1), limits=c(0,200)) +
+  geom_point(aes(lon, lat),
+             shape = 21,
+             stroke = 5,
+             size = 1,
+             color = "green",
+             fill = "green4",
+             alpha = 0.2,
+             data = internal_medicine_003,
+             position = position_jitter(w = 0.002, h = 0.002)) +
+  facet_wrap(~Carrier, ncol = 4) +
+  xlab(" ") +
+  ylab(NULL) +
+  ggtitle(paste0('2018 ', '"', selected_specialty, '"', ' Providers')) +
+  geom_text(data = statewide_count_internal_medicine_003, aes(x = -107, y = 41, label = str_c(count, " Providers"))) +
+  labs(caption = "\n\n  Graphic by Colorado Health Insurance Insider | @lukkyjay                                                                                          Source: SERFF") +
+  theme_provider_maps +
+  ggsave(filename = "Plots/internal_medicine_003.png", width = 9, height = 5, dpi = 1200)
